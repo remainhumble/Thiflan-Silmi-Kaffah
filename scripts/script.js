8ff85e84b13f99fa20788044e2a65d8d66cd9797
Thiflan <thiflan120699@gmail.com>
2024-07-07 19:27:05 +1200

    footer updt

--- scripts/script.js
+++ scripts/script.js
@@ -18,49 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-});
+// Get the current year
+const currentYear = new Date().getFullYear();
 
-// Get all needed elements from the DOM
-const contactForm = document.getElementById("contact-form");
-const submitBtn = document.getElementsByClassName("button");
-const nameInput = document.getElementById("user_name");
-const emailInput = document.getElementById("user_email");
-const messageInput = document.getElementById("message");
+// Find the HTML element with the ID 'year'
+const yearElement = document.getElementById("year");
 
-// Get needed data from Email JS
-const publicKey = "IU6uYeBu4OkdpftJ_";
-const serviceID = "service_sd86wii";
-const templateID = "template_i4070xt";
-
-// Initialize email JS with publicKey
-emailjs.init(publicKey);
-
-// Add submit event to the form
-contactForm.addEventListener("submit", e => {
-  // Prevent form default behaviour
-  e.preventDefault();
-  // Change submit button text
-  submitBtn.innerText = "Just a Moment...";
-  // Get all input field values
-  const inputFields = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value
-  }
-  /* Send the email
-  (Add service, template ID, and input field values) */
-  emailjs.send(serviceID, templateID, inputFields)
-    .then(() => {
-// Change submit button text
-submitBtn.innerText = "Message Sent Successfully";
-// Clear out all input fields
-nameInput.value = "";
-emailInput.value = "";
-messageInput.value = "";
-    }, (error) => {
-      // Log the error
-      console.log(error);
-      // Change submit button text
-      submitBtn.innerText = "Something went wrong";
-    });
+// Set the text content of the 'year' element to the current year
+yearElement.textContent = currentYear;
 });
